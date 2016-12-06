bc9ee907de2c358062d3290771164fdee0452ec1
Fotios Lindiakos <flindiakos@shutterstock.com>
2016-12-05 23:12:00 -0500

    Cleanup day 6

--- 2016/day_5/index.js
+++ 2016/day_5/index.js
@@ -8,58 +8,27 @@ function getHash(input, id) {
 }
 
 function* hashSequence(input, id = 0) {
-  let i = 0;
+  let i = id;
   while (true) {
-    yield { hash: getHash(input, id + i), i: id + i };
     i += 1;
+    yield getHash(input, i);
   }
 }
 
-const p = new Puzzle({ delimiter: /,\s*/ });
-
-p.A = ([input, length, numZeroes]) => {
-  const zeroString = Array(Number.parseInt(numZeroes, 10) + 1).join('0');
-  const hasher = hashSequence(input);
-  const pass = [];
-
-  for (const { hash } of hasher) {
-    if (hash.startsWith(zeroString)) {
-      pass.push(hash[numZeroes]);
-      console.log({ hash, pass });
-    }
-    if (pass.filter(x => !!x).length === Number.parseInt(length, 10)) {
-      break;
-    }
-  }
-
-  return pass.join('');
-};
-
-function* validSeq({ input, args }) {
-  const [length, numZeroes, starting] = [args, 0].map(x => Number.parseInt(x, 10));
-
-  const zeroString = Array(numZeroes + 1).join('0');
-  const hasher = hashSequence(input, starting);
-  const pass = new Array(length);
-}
-
-p.B = ([input, ...args]) => {
-  const g = validSeq({ input, args });
+const findPass = validate => ([input, ...args]) => {
   const [length, numZeroes, starting] = [...args, 0].map(x => Number.parseInt(x, 10));
 
   const zeroString = Array(numZeroes + 1).join('0');
-  const hasher = hashSequence(input, starting);
-  const pass = new Array(length);
+  let pass = new Array(length);
 
-  for (const { hash } of hasher) {
+  const hasher = hashSequence(input, starting);
+
+  for (const hash of hasher) {
     if (hash.startsWith(zeroString)) {
       const index = hash[numZeroes];
       const val = hash[numZeroes + 1];
 
-      if (index < length && typeof pass[index] === 'undefined') {
-        pass[index] = val;
-      }
-      console.log({ hash, pass, index, val, length });
+      pass = validate({ index, length, pass, val });
     }
     if (pass.filter(x => !!x).length === length) {
       break;
@@ -69,8 +38,17 @@ p.B = ([input, ...args]) => {
   return pass.join('');
 };
 
-// p.run({ data: 'abbhdwsy,8,5', type: 'A' });
-// p.run({ data: 'abbhdwsy,8,5', type: 'B' });
+const p = new Puzzle({ delimiter: /,\s*/ });
 
-// p.run({ data: 'abbhdwsy,2,5', type: 'A' });
+p.A = findPass(({ pass, index: val }) => [...pass, val]);
+
+p.B = findPass(({ index, length, pass, val }) => {
+  const pass1 = pass;
+  if (index < length && typeof pass[index] === 'undefined') {
+    pass1[index] = val;
+  }
+  return pass1;
+});
+
+p.run({ data: 'abbhdwsy,2,5', type: 'A' });
 p.run({ data: 'abbhdwsy,2,5', type: 'B' });
